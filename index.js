fa5607a98f5093bac9aa3066270c7e306fa5693d
williamlacy <williamlacy@ucds.email>
2017-11-22 12:18:01 +0000

    add error messaging for gulp

--- index.js
+++ index.js
@@ -7,22 +7,24 @@ const path = require('path');
 const LintTargetBlank = require('lint-target-blank');
 // The main function for the plugin – what the user calls – should return
 // a stream.
-const lintTargetBlankPlugin = function(opts) {
+const lintTargetBlankPlugin = function (opts) {
   // Create and return a stream that, for each file
   const lintTargetBlank = new LintTargetBlank(opts);
-  return map(function(file, cb) {
+  return map(function (file, cb) {
     lintTargetBlank.lintFile(file.path)
       .then(errors => {
         const lintErrors = errors
           .filter(e => e.type === 'error')
           .map(e => `\t${path.relative(path.resolve(process.cwd()), file.path)}: ${e.element}`)
           .join('\n');
-        if (lintErrors) cb(new gutil.PluginError('gulp-lint-target-blank', {
-          name: 'TargetBlankLintError',
-          message: `The following elements require rel="noreferrer nofollow"\n\n${lintErrors}`
-        }));
+        if (lintErrors) {
+          cb(new gutil.PluginError('gulp-lint-target-blank', {
+            name: 'TargetBlankLintError',
+            message: `The following elements require rel="noreferrer nofollow"\n\n${lintErrors}`,
+          }));
+        }
       });
-  })
+  });
 };
 
 // Export the plugin main function
